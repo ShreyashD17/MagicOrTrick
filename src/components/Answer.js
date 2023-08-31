c78ef0d1917e6fae7b9c3170da6e04db4736fe69
ShreyashD17 <110217090+ShreyashD17@users.noreply.github.com>
2023-08-31 17:19:43 +0530

    Update Answer.js

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -18,7 +18,7 @@ const Answer = ({answer,cards,setAnswer}) => {
     <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
     <h3 style={{textAlign:"center",fontFamily: "Kalam, cursive",textShadow:"2px 2px 10px #a5a5ee"}}>Your selected card is :</h3>
     <img src={imagePath} alt={cardName} className='finalCard animated' style={{marginTop:"50px",marginBottom:"80px"}}/>
-    <h3 className='credit'>Thanks For Playing :) Game Made By Yugal Pradhan </h3>
+    <h3 className='credit'>Thanks For Playing :)</h3>
     <h3 className='again' style={{textAlign:"center",fontFamily: "Kalam, cursive",textShadow:"2px 2px 10px #a5a5ee",marginTop:"20px"}}>Wanna play again?</h3>
     <div onClick={handleClick} className="button">Yes</div>
     </div>
